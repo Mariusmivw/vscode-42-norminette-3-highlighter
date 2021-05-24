ee798c3d201de8ab1988130743357e0f12a79970
Marius van Wijk <mvan-wij@student.codam.nl>
2021-05-24 13:04:39 +0200

    caching (non) ignored files

--- src/normignore.ts
+++ src/normignore.ts
@@ -1,23 +1,23 @@
 import { readFileSync } from 'fs'
 import ignore, { Ignore } from 'ignore'
-import { dirname, relative } from 'path'
+import * as path from 'path'
 import * as vscode from 'vscode'
 import { log } from './extension'
 
-export type IgnoreSystem = { [workspace: string]: { [folder: string]: Ignore } }
+export type IgnoreSystem = { ignored: string[], notIgnored: string[], workspaces: { [workspace: string]: { [folder: string]: Ignore } } }
 
 export function initNormignore(): IgnoreSystem {
-	const ignores: IgnoreSystem = {}
+	const ignores: IgnoreSystem = { ignored: [], notIgnored: [], workspaces: {} }
 
 	vscode.workspace.findFiles('**/.normignore').then((fileUris) => {
 		for (const fileUri of fileUris) {
 			const workspace = vscode.workspace.getWorkspaceFolder(fileUri).uri.path
 			if (!workspace) return
 			log('adding: ' + fileUri.path)
-			const ignorePath = dirname(relative(workspace, fileUri.path))
-			if (!ignores[workspace])
-				ignores[workspace] = {}
-			ignores[workspace][ignorePath] = ignore().add(readFileSync(fileUri.fsPath).toString())
+			const ignorePath = path.dirname(path.relative(workspace, fileUri.path))
+			if (!ignores.workspaces[workspace])
+				ignores.workspaces[workspace] = {}
+			ignores.workspaces[workspace][ignorePath] = ignore().add(readFileSync(fileUri.fsPath).toString())
 		}
 	})
 
@@ -26,27 +26,33 @@ export function initNormignore(): IgnoreSystem {
 		const workspace = vscode.workspace.getWorkspaceFolder(fileUri).uri.path
 		if (!workspace) return
 		log('removing: ' + fileUri.path)
-		const ignorePath = dirname(relative(workspace, fileUri.path))
-		if (ignores[workspace] && ignores[workspace][ignorePath])
-			delete ignores[workspace][ignorePath]
+		ignores.ignored = []
+		ignores.notIgnored = []
+		const ignorePath = path.dirname(path.relative(workspace, fileUri.path))
+		if (ignores.workspaces[workspace] && ignores.workspaces[workspace][ignorePath])
+			delete ignores.workspaces[workspace][ignorePath]
 	})
 	watcher.onDidCreate((fileUri) => {
 		const workspace = vscode.workspace.getWorkspaceFolder(fileUri).uri.path
 		if (!workspace) return
 		log('adding: ' + fileUri.path)
-		const ignorePath = dirname(relative(workspace, fileUri.path))
-		if (!ignores[workspace])
-			ignores[workspace] = {}
-		ignores[workspace][ignorePath] = ignore().add(readFileSync(fileUri.fsPath).toString())
+		ignores.ignored = []
+		ignores.notIgnored = []
+		const ignorePath = path.dirname(path.relative(workspace, fileUri.path))
+		if (!ignores.workspaces[workspace])
+			ignores.workspaces[workspace] = {}
+		ignores.workspaces[workspace][ignorePath] = ignore().add(readFileSync(fileUri.fsPath).toString())
 	})
 	watcher.onDidChange((fileUri) => {
 		const workspace = vscode.workspace.getWorkspaceFolder(fileUri).uri.path
 		if (!workspace) return
 		log('changing / replacing: ' + fileUri.path)
-		const ignorePath = dirname(relative(workspace, fileUri.path))
-		if (!ignores[workspace])
-			ignores[workspace] = {}
-		ignores[workspace][ignorePath] = ignore().add(readFileSync(fileUri.fsPath).toString())
+		ignores.ignored = []
+		ignores.notIgnored = []
+		const ignorePath = path.dirname(path.relative(workspace, fileUri.path))
+		if (!ignores.workspaces[workspace])
+			ignores.workspaces[workspace] = {}
+		ignores.workspaces[workspace][ignorePath] = ignore().add(readFileSync(fileUri.fsPath).toString())
 	})
 
 	return ignores
@@ -54,24 +60,29 @@ export function initNormignore(): IgnoreSystem {
 
 export function isIgnored(fileUri: vscode.Uri, ignores: IgnoreSystem): boolean {
 	const workspace = vscode.workspace.getWorkspaceFolder(fileUri).uri.path
-	if (!workspace || !ignores[workspace]) return
-	const filePath = relative(workspace, fileUri.path)
+	if (!workspace || !ignores.workspaces[workspace])
+		return false
+	const filePath = path.relative(workspace, fileUri.path)
+	if (ignores.ignored.includes(filePath))
+		return true
+	if (ignores.notIgnored.includes(filePath))
+		return false
 	const parts = filePath.split('/')
 	let folder: string
 	for (let dirs = 1; dirs < parts.length + 1; dirs++) {
-		const folderToCheck = dirname(parts.slice(0, dirs).join('/'))
-		if (ignores[workspace][folderToCheck]) {
-			if (folder && ignores[workspace][folder].ignores(relative(folder, folderToCheck) + '/'))
+		const folderToCheck = path.dirname(parts.slice(0, dirs).join('/'))
+		if (ignores.workspaces[workspace][folderToCheck]) {
+			if (folder && ignores.workspaces[workspace][folder].ignores(path.relative(folder, folderToCheck) + '/'))
 				break
 			folder = folderToCheck
 		}
 	}
 	if (!folder)
 		return false
-	let result = ignores[workspace][folder].test(relative(folder, filePath))
+	let result = ignores.workspaces[workspace][folder].test(path.relative(folder, filePath))
 	while (folder != '.' && !result.ignored && !result.unignored) {
-		folder = dirname(folder)
-		result = ignores[workspace][folder].test(relative(folder, filePath))
+		folder = path.dirname(folder)
+		result = ignores.workspaces[workspace][folder].test(path.relative(folder, filePath))
 	}
 	return result.ignored
 }
