bf7270b0b3375525131444c4e6d569b26893f1cf
SirMorfield <joppe.koers@gmail.com>
2021-08-01 00:19:53 +0200

    handle breaking 3.3.1 to 3.3.2 norminette change by ignoring the "Missing or invalid header. Header are being reintroduced as a mandatory part of your files. This is not yet an error." error line

--- src/norminette.ts
+++ src/norminette.ts
@@ -9,15 +9,21 @@ export type NormInfo = {
 	errorText: string
 }
 
-function normDecrypt(normLine: string): NormInfo {
-	const [fullText, error, line, col, errorText] = normLine.match(/\s*([A-Z_]*)\s*\(line:\s*(\d*),\s*col:\s*(\d+)\):\s*(.*)/)
-
-	return {
-		fullText,
-		error,
-		line: parseInt(line) - 1,
-		col: parseInt(col) - 1,
-		errorText: errorText[0].toUpperCase() + errorText.slice(1)
+function normDecrypt(normLine: string): NormInfo | undefined {
+	try {
+		const [fullText, error, line, col, errorText] = normLine.match(/\s*([A-Z_]*)\s*\(line:\s*(\d*),\s*col:\s*(\d+)\):\s*(.*)/)
+		if (!fullText || !error || !line || !col || !errorText)
+			return null
+		const result = {
+			fullText,
+			error,
+			line: parseInt(line) - 1,
+			col: parseInt(col) - 1,
+			errorText: errorText[0].toUpperCase() + errorText.slice(1)
+		}
+		return (result)
+	} catch (e) {
+		return null
 	}
 }
 
@@ -25,6 +31,7 @@ export function execNorminette(filename: string, command: string): Promise<NormI
 	return new Promise<NormInfo[]>((resolve, reject) => {
 		const lines: string[] = []
 		const normDecrypted: NormInfo[] = []
+		console.log('aaaaaaa')
 		const proc = exec(`${command} '${filename}'`, (error, stdout, stderr) => {
 			stdout.split('\n').forEach((error_line, i) => {
 				if (i == 0)
@@ -38,7 +45,9 @@ export function execNorminette(filename: string, command: string): Promise<NormI
 				lines.pop()
 				lines.forEach((error_line) => {
 					log(`line: ${error_line}`)
-					normDecrypted.push(normDecrypt(error_line))
+					const decrypted = normDecrypt(error_line)
+					if (decrypted)
+						normDecrypted.push(decrypted)
 				})
 				// console.log(normDecrypted)
 				resolve(normDecrypted)
