3916384b2701603e3405199d5344bdd54b6f4b9f
SirMorfield <joppe.koers@gmail.com>
2021-08-02 17:48:36 +0200

    Using async instead of callbacks

--- src/norminette.ts
+++ src/norminette.ts
@@ -1,6 +1,14 @@
 import { exec } from 'child_process'
 import { log } from './extension'
 
+async function execAsync(command): Promise<{ stdout: string, stderr: string } | null> {
+	return new Promise((resolve, reject) => {
+		exec(`${command}`, (error, stdout, stderr) => {
+			resolve({ stdout, stderr })
+		})
+	})
+}
+
 export type NormInfo = {
 	fullText: string,
 	error: string,
@@ -9,7 +17,7 @@ export type NormInfo = {
 	errorText: string
 }
 
-function normDecrypt(normLine: string): NormInfo | undefined {
+function normDecrypt(normLine: string): NormInfo {
 	try {
 		const [fullText, error, line, col, errorText] = normLine.match(/\s*([A-Z_]*)\s*\(line:\s*(\d*),\s*col:\s*(\d+)\):\s*(.*)/)
 		if (!fullText || !error || !line || !col || !errorText)
@@ -22,40 +30,21 @@ function normDecrypt(normLine: string): NormInfo | undefined {
 			errorText: errorText[0].toUpperCase() + errorText.slice(1)
 		}
 		return (result)
-	} catch (e) {
+	}
+	catch (e) {
 		return null
 	}
 }
 
-export function execNorminette(filename: string, command: string): Promise<NormInfo[] | null> {
-	return new Promise<NormInfo[]>((resolve, reject) => {
-		const lines: string[] = []
-		const normDecrypted: NormInfo[] = []
-		const proc = exec(`${command} '${filename}'`, (error, stdout, stderr) => {
-			stdout.split('\n').forEach((error_line, i) => {
-				if (i == 0)
-					return
-				log(`error line: ${error_line}`)
-				lines.push(error_line)
-			})
-		})
-		proc.on('close', (exitCode) => {
-			try {
-				lines.pop()
-				lines.forEach((error_line) => {
-					log(`line: ${error_line}`)
-					const decrypted = normDecrypt(error_line)
-					if (decrypted)
-						normDecrypted.push(decrypted)
-				})
-				// console.log(normDecrypted)
-				resolve(normDecrypted)
-			}
-			catch (e) {
-				resolve(null)
-				// console.log('error')
-				// console.log(e)
-			}
-		})
-	})
+export async function execNorminette(filename: string, command: string): Promise<NormInfo[]> {
+	const { stdout } = await execAsync(`${command} '${filename}'`)
+	const lines = stdout.split('\n').slice(1, -1)
+	let normDecrypted = []
+	for (const line of lines) {
+		log(`line: ${line}`)
+		const decrypted = normDecrypt(line)
+		if (decrypted)
+			normDecrypted.push(decrypted)
+	}
+	return normDecrypted
 }
